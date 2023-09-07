e2e78067d4681ad83b59aeccc9fca0cd78da9419
HamideT4 <hamidemahamat20@gmail.com>
2023-09-07 17:34:25 +0100

    Update file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
-#include <studio.h>
+#include <stdio.h>
 
+/**
+ * main - Entry point of the program that print a text
+ *
+ * Return 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
-} 
+	return (0);
+}
